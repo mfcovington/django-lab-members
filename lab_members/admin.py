7be1fe725bafbbf1a87baaa6a710c97faba42ca0
Mike Covington <mfcovington@gmail.com>
2014-11-25 15:04:26 -0800

    Register PositionAdmin and ScientistAdmin

--- lab_members/admin.py
+++ lab_members/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from lab_members.models import Position, Scientist
 
-# Register your models here.
+class PositionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Position, PositionAdmin)
+
+class ScientistAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Scientist, ScientistAdmin)
