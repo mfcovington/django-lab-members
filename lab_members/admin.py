86021c10e2c5db4a916dc3d95e19ae79efa1dad2
Mike Covington <mfcovington@gmail.com>
2014-11-25 16:28:51 -0800

    Customize lab_member admin

--- lab_members/admin.py
+++ lab_members/admin.py
@@ -2,11 +2,22 @@ from django.contrib import admin
 from lab_members.models import Position, Scientist
 
 class PositionAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['title']
 
 admin.site.register(Position, PositionAdmin)
 
 class ScientistAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Basic Info', {'fields': ['full_name', 'title']}),
+        ('Advanced',   {'fields': ['slug'], 'classes': ['collapse']}),
+    ]
+
+    list_display = ['full_name', 'title']
+    list_filter = ['title']
+    search_fields = ['full_name']
+
     prepopulated_fields = {"slug": ("full_name",)}
 
 admin.site.register(Scientist, ScientistAdmin)
+
+admin.site.site_header = 'Lab Member Administration'
