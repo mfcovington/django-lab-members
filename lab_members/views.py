ee5730abf489de4c1f3d6ca1d60164f0a613b922
Mike Covington <mfcovington@gmail.com>
2015-04-02 01:15:11 -0700

    Redirect to previous page when logging in/out

--- lab_members/views.py
+++ lab_members/views.py
@@ -8,7 +8,13 @@ class ScientistListView(ListView):
         context = super(ScientistListView, self).get_context_data(**kwargs)
         context['current_list'] = Scientist.objects.filter(current=True)
         context['alumni_list'] = Scientist.objects.filter(current=False)
+        context['request'] = self.request
         return context
 
 class ScientistDetailView(DetailView):
     model = Scientist
+
+    def get_context_data(self, **kwargs):
+        context = super(ScientistDetailView, self).get_context_data(**kwargs)
+        context['request'] = self.request
+        return context
