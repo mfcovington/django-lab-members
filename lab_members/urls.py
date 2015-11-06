4c019b149510f74f614240d031ef03f96fa017e4
Mike Covington <mfcovington@gmail.com>
2015-11-05 21:59:58 -0800

    Add trailing '/' for scientist detail URL pattern

--- lab_members/urls.py
+++ lab_members/urls.py
@@ -3,5 +3,5 @@ from lab_members.views import ScientistListView, ScientistDetailView
 
 urlpatterns = patterns('',
     url(r'^$', ScientistListView.as_view(), name='scientist_list'),
-    url(r'^(?P<slug>[^/]+)$', ScientistDetailView.as_view(), name='scientist_detail'),
+    url(r'^(?P<slug>[^/]+)/$', ScientistDetailView.as_view(), name='scientist_detail'),
 )
